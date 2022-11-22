8098051a1d31177ee26c03b14a474396a8d71396
Ignas <>
2022-11-22 22:54:06 +0200

    Add Xcode project

---
+++ pawn-test-app/pawn-test-app/pawn_test_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  pawn_test_appApp.swift
+//  pawn-test-app
+//
+//  Created by Ignas on 2022-11-22.
+//
+
+import SwiftUI
+
+@main
+struct pawn_test_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
